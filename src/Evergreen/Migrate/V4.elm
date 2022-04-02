a7d6f81e1abbc0a9745845192529fe6ed57cdda1
Amir Rad <ahmr10@gmail.com>
2022-04-02 11:36:34 +0100

    Preparing for v4

---
+++ src/Evergreen/Migrate/V4.elm
@@ -0,0 +1,37 @@
+module Evergreen.Migrate.V4 exposing (..)
+
+import Evergreen.V3.Types as Old
+import Evergreen.V4.Types as New
+import Lamdera.Migrations exposing (..)
+import Array as Arr
+import Dict
+
+
+frontendModel : Old.FrontendModel -> ModelMigration New.FrontendModel New.FrontendMsg
+frontendModel old = ModelMigrated (
+    { guesses = Arr.fromList []
+    , clientId = ""
+    , possibleGuesses= Nothing
+    , possibleGuessesCount= Nothing
+    }, Cmd.none)
+
+
+
+backendModel : Old.BackendModel -> ModelMigration New.BackendModel New.BackendMsg
+backendModel old = ModelMigrated ({clientGuesses = Dict.empty}, Cmd.none)
+
+
+frontendMsg : Old.FrontendMsg -> MsgMigration New.FrontendMsg New.FrontendMsg
+frontendMsg old = MsgOldValueIgnored
+
+
+toBackend : Old.ToBackend -> MsgMigration New.ToBackend New.BackendMsg
+toBackend old = MsgOldValueIgnored
+
+
+backendMsg : Old.BackendMsg -> MsgMigration New.BackendMsg New.BackendMsg
+backendMsg old = MsgOldValueIgnored
+
+
+toFrontend : Old.ToFrontend -> MsgMigration New.ToFrontend New.FrontendMsg
+toFrontend old = MsgOldValueIgnored
---
+++ src/Evergreen/V4/Types.elm
@@ -0,0 +1,45 @@
+module Evergreen.V4.Types exposing (..)
+
+import Array
+import Dict
+import Lamdera
+
+
+type Guess a
+    = Elim a
+    | NotAt a Int
+    | At a Int
+
+
+type alias FrontendModel =
+    { guesses : Array.Array (Array.Array (Guess Char))
+    , clientId : String
+    , possibleGuesses : Maybe (List String)
+    , possibleGuessesCount : Maybe Int
+    }
+
+
+type alias BackendModel =
+    { clientGuesses : Dict.Dict String (List (Guess Char))
+    }
+
+
+type FrontendMsg
+    = EnterKeyPressed
+    | DelKeyPressed
+    | CharKeyPressed Char
+    | GuessStateChange Int Int
+    | FNoop
+
+
+type ToBackend
+    = SubmitGuesses (List (Guess Char))
+
+
+type BackendMsg
+    = ClientConnected Lamdera.SessionId Lamdera.ClientId
+
+
+type ToFrontend
+    = FilteredWords (List String)
+    | FilteredWordsCount Int
