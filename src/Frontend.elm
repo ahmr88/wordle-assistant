78654fbdc92a42b1dac22fb3b3c7514c941d5d92
Amir Rad <ahmr10@gmail.com>
2022-04-04 18:33:36 +0100

    Updates

--- src/Frontend.elm
+++ src/Frontend.elm
@@ -29,7 +29,7 @@ app =
         , updateFromBackend = updateFromBackend
         , view =
             \model ->
-                { title = "Wordle Assistant ;)"
+                { title = "Wordle Helper"
                 , body = [ view model ]
                 }
         , subscriptions = \_ -> Sub.none
